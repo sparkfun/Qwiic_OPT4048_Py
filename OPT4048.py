f41f0695c7149e0f2320d834a44562be9d9f1883
Elias Santistevan <elias.santistevan@sparkfun.com>
2023-11-10 14:55:23 -0700

    Adds unions of register values....
    * Adds a few missing functions from "inherited" c++ lib

--- OPT4048.py
+++ OPT4048.py
@@ -1,18 +1,23 @@
+from dataclasses import dataclass
+import qwiic_i2c
+import time
+import OPT4048_Register.py
 
+
+@dataclass
 class SFEColor:
-    def __init__(self):
-        self.red = 0
-        self.green = 0
-        self.blue = 0
-        self.white = 0
-        self.counterR = 0
-        self.counterG = 0
-        self.counterB = 0
-        self.counterW = 0
-        self.CRCR = 0
-        self.CRCG = 0
-        self.CRCB = 0
-        self.CRCW = 0
+    red : int = 0
+    green : int = 0
+    blue : int = 0
+    white : int = 0
+    counterR : int = 0
+    counterG : int = 0
+    counterB : int = 0
+    counterW : int = 0
+    CRCR : int = 0
+    CRCG : int = 0
+    CRCB : int = 0
+    CRCW : int = 0
 
 
 class CRCBits:
@@ -55,73 +60,72 @@ class MantissaBits:
         self.bit19 = 0
 
 class QwOpt4048:
-    def __init__(self):
-        self._sfeBus = None
-        self._i2cAddress = None
-        self.crcEnabled = False
 
-    def init(self):
-        if not self._sfeBus.ping(self._i2cAddress):
-            return False
+    device_name = _DEFAULT_NAME
+    available_addresses = _AVAILABLE_I2C_ADDRESS
+    
+    self.opt4048_range_t = opt4048RangeT
+    self.opt4048_conversion_time_t = opt4048ConversionTimeT
+    self.opt4048_operation_mode_t = opt4048OperationModeT
+    self.sfe_color_t = SFEColor
+    
+    self.opt4048_reg_control = 
 
-        if self.getDeviceID() != OPT4048_DEVICE_ID:
+    def __init__(self, address=None, i2c_driver=None):
+
+        # Did the user specify an I2C address?
+        self.address = self.available_addresses[0] if address is None else address
+
+        # load the I2C driver if one isn't provided
+
+        if i2c_driver is None:
+            self._i2c = qwiic_i2c.getI2CDriver()
+            if self._i2c is None:
+                print("Unable to load I2C driver for this platform.")
+                return
+        else:
+            self._i2c = i2c_driver
+
+
+    def is_connected(self):
+        """
+            Determine if a BME280 device is conntected to the system..
+
+            :return: True if the device is connected, otherwise False.
+            :rtype: bool
+
+        """
+        return qwiic_i2c.isDeviceConnected(self.address)
+
+    connected = property(is_connected)
+
+    def begin(self):
+
+        if self.get_device_id() != OPT4048_DEVICE_ID:
             return False
 
         return True
 
-    def is_connected(self):
-        if self.getDeviceID() != OPT4048_DEVICE_ID:
-            return False
-        else:
-            return True
 
     def get_device_id(self):
-        buff = [0, 0]
-        unique_id = 0
 
-        retVal = self.read_register_region(SFE_OPT4048_REGISTER_DEVICE_ID, buff)
-
-        id_reg = ((buff[0] << 8) | buff[1])
-
-        unique_id = (id_reg.DIDH << 2) | id_reg.DIDL
-
-        if retVal != 0:
-            return 0
+        unique_id = self._i2c.readWord(self.address, SFE_OPT4048_REGISTER_DEVICE_ID)
 
         return unique_id
 
-    def set_communication_bus(self, the_bus, i2c_address):
-        self._sfeBus = the_bus
-        self._i2cAddress = i2c_address
-
-    def set_communication_bus(self, the_bus):
-        self._sfeBus = the_bus
-
-    def write_register_region(self, offset, data, length):
-        return self._sfeBus.write_register_region(self._i2cAddress, offset, data, length)
-
-    def read_register_region(self, offset, data, length):
-        return self._sfeBus.read_register_region(self._i2cAddress, offset, data, length)
-
     def set_basic_setup(self):
-        self.set_range(RANGE_36LUX)
-        self.set_conversion_time(CONVERSION_TIME_200MS)
-        self.set_operation_mode(OPERATION_MODE_CONTINUOUS)
+        self.set_range(opt4048_range_t.RANGE_36LUX)
+        self.set_conversion_time(opt4048_range_t.CONVERSION_TIME_200MS)
+        self.set_operation_mode(opt4048_range_t.OPERATION_MODE_CONTINUOUS)
 
     def set_range(self, range):
-        buff = [0, 0]
-        retVal = self.read_register_region(SFE_OPT4048_REGISTER_CONTROL, buff)
 
-        if retVal != 0:
-            return False
-
-        controlReg = ((buff[0] << 8) | buff[1])
-        controlReg.range = range
+        reg_range = self._i2c.readWord(self.address, SFE_OPT4048_REGISTER_CONTROL)
 
         buff[0] = controlReg.word >> 8
         buff[1] = controlReg.word
 
-        retVal = self.write_register_region(SFE_OPT4048_REGISTER_CONTROL, buff)
+        retVal = self._i2c.writeWord(SFE_OPT4048_REGISTER_CONTROL)
 
         if retVal != 0:
             return False
@@ -132,7 +136,7 @@ class QwOpt4048:
         buff = [0, 0]
         controlReg = opt4048_reg_control_t()
 
-        self.read_register_region(SFE_OPT4048_REGISTER_CONTROL, buff)
+        self._i2c.readWord(self.address, SFE_OPT4048_REGISTER_CONTROL)
 
         controlReg.word = (buff[0] << 8) | buff[1]
 
@@ -140,7 +144,7 @@ class QwOpt4048:
 
     def set_conversion_time(self, time):
         buff = bytearray([0, 0])
-        retVal = self.readRegisterRegion(SFE_OPT4048_REGISTER_CONTROL, buff)
+        retVal = self._i2c.readWord(self.address, SFE_OPT4048_REGISTER_CONTROL)
 
         if retVal != 0:
             return False
@@ -153,7 +157,7 @@ class QwOpt4048:
         buff[0] = controlReg >> 8
         buff[1] = controlReg & 0xFF
 
-        retVal = self.writeRegisterRegion(SFE_OPT4048_REGISTER_CONTROL, buff)
+        retVal = self.writeRegisterRegion(SFE_OPT4048_REGISTER_CONTROL
 
         if retVal != 0:
             return False
@@ -162,7 +166,7 @@ class QwOpt4048:
 
     def get_conversion_time(self):
         buff = bytearray([0, 0])
-        self.readRegisterRegion(SFE_OPT4048_REGISTER_CONTROL, buff)
+        self._i2c.readWord(self.address, SFE_OPT4048_REGISTER_CONTROL)
 
         controlReg = (buff[0] << 8) | buff[1]
 
@@ -170,7 +174,7 @@ class QwOpt4048:
 
     def set_qwake(self, enable):
         buff = bytearray([0, 0])
-        retVal = self.readRegisterRegion(SFE_OPT4048_REGISTER_CONTROL, buff)
+        retVal = self._i2c.readWord(self.address, SFE_OPT4048_REGISTER_CONTROL)
 
         if retVal != 0:
             return False
@@ -185,7 +189,7 @@ class QwOpt4048:
         buff[0] = controlReg >> 8
         buff[1] = controlReg & 0xFF
 
-        retVal = self.writeRegisterRegion(SFE_OPT4048_REGISTER_CONTROL, buff)
+        retVal = self.writeRegisterRegion(SFE_OPT4048_REGISTER_CONTROL
 
         if retVal != 0:
             return False
@@ -194,7 +198,7 @@ class QwOpt4048:
 
     def get_qwake(self):
         buff = bytearray([0, 0])
-        self.readRegisterRegion(SFE_OPT4048_REGISTER_CONTROL, buff)
+        self._i2c.readWord(self.address, SFE_OPT4048_REGISTER_CONTROL)
 
         controlReg = (buff[0] << 8) | buff[1]
 
@@ -202,7 +206,7 @@ class QwOpt4048:
 
     def set_operation_mode(self, mode):
         buff = bytearray([0, 0])
-        retVal = self.readRegisterRegion(SFE_OPT4048_REGISTER_CONTROL, buff)
+        retVal = self._i2c.readWord(self.address, SFE_OPT4048_REGISTER_CONTROL)
 
         if retVal != 0:
             return False
@@ -215,7 +219,7 @@ class QwOpt4048:
         buff[0] = controlReg >> 8
         buff[1] = controlReg & 0xFF
 
-        retVal = self.writeRegisterRegion(SFE_OPT4048_REGISTER_CONTROL, buff)
+        retVal = self.writeRegisterRegion(SFE_OPT4048_REGISTER_CONTROL
 
         if retVal != 0:
             return False
@@ -224,7 +228,7 @@ class QwOpt4048:
 
     def get_operation_mode(self):
         buff = bytearray([0, 0])
-        self.readRegisterRegion(SFE_OPT4048_REGISTER_CONTROL, buff)
+        self._i2c.readWord(self.address, SFE_OPT4048_REGISTER_CONTROL)
 
         controlReg = (buff[0] << 8) | buff[1]
 
@@ -232,7 +236,7 @@ class QwOpt4048:
 
     def set_int_latch(self, enable):
         buff = bytearray([0, 0])
-        ret_val = self.read_register_region(SFE_OPT4048_REGISTER_CONTROL, buff)
+        ret_val = self._i2c.readWord(self.address, SFE_OPT4048_REGISTER_CONTROL)
 
         if ret_val != 0:
             return False
@@ -243,7 +247,7 @@ class QwOpt4048:
         buff[0] = control_reg >> 8
         buff[1] = control_reg
 
-        ret_val = self.write_register_region(SFE_OPT4048_REGISTER_CONTROL, buff)
+        ret_val = self._i2c.writeWord(SFE_OPT4048_REGISTER_CONTROL
 
         if ret_val != 0:
             return False
@@ -252,7 +256,7 @@ class QwOpt4048:
 
     def get_int_latch(self):
         buff = bytearray([0, 0])
-        self.read_register_region(SFE_OPT4048_REGISTER_CONTROL, buff)
+        self._i2c.readWord(self.address, SFE_OPT4048_REGISTER_CONTROL)
 
         control_reg = (buff[0] << 8) | buff[1]
 
@@ -260,7 +264,7 @@ class QwOpt4048:
 
     def set_int_active_high(self, enable):
         buff = bytearray([0, 0])
-        ret_val = self.read_register_region(SFE_OPT4048_REGISTER_CONTROL, buff)
+        ret_val = self._i2c.readWord(self.address, SFE_OPT4048_REGISTER_CONTROL)
 
         if ret_val != 0:
             return False
@@ -271,7 +275,7 @@ class QwOpt4048:
         buff[0] = int_reg >> 8
         buff[1] = int_reg
 
-        ret_val = self.write_register_region(SFE_OPT4048_REGISTER_CONTROL, buff)
+        ret_val = self._i2c.writeWord(SFE_OPT4048_REGISTER_CONTROL
 
         if ret_val != 0:
             return False
@@ -280,7 +284,7 @@ class QwOpt4048:
 
     def get_int_active_high(self):
         buff = bytearray([0, 0])
-        self.read_register_region(SFE_OPT4048_REGISTER_CONTROL, buff)
+        self._i2c.readWord(self.address, SFE_OPT4048_REGISTER_CONTROL)
 
         int_reg = (buff[0] << 8) | buff[1]
 
@@ -288,7 +292,7 @@ class QwOpt4048:
 
     def set_int_input(self, enable):
         buff = bytearray([0, 0])
-        ret_val = self.read_register_region(SFE_OPT4048_REGISTER_INT_CONTROL, buff)
+        ret_val = self._i2c.readWord(self.address, SFE_OPT4048_REGISTER_INT_CONTROL)
 
         if ret_val != 0:
             return False
@@ -299,7 +303,7 @@ class QwOpt4048:
         buff[0] = int_reg >> 8
         buff[1] = int_reg
 
-        ret_val = self.write_register_region(SFE_OPT4048_REGISTER_INT_CONTROL, buff)
+        ret_val = self._i2c.writeWord(SFE_OPT4048_REGISTER_INT_CONTROL
 
         if ret_val != 0:
             return False
@@ -308,7 +312,7 @@ class QwOpt4048:
 
     def get_int_input_enable(self):
         buff = bytearray([0, 0])
-        self.read_register_region(SFE_OPT4048_REGISTER_INT_CONTROL, buff)
+        self._i2c.readWord(self.address, SFE_OPT4048_REGISTER_INT_CONTROL)
 
         int_reg = (buff[0] << 8) | buff[1]
 
@@ -316,7 +320,7 @@ class QwOpt4048:
 
     def set_int_mechanism(self, mechanism):
         buff = bytearray([0, 0])
-        ret_val = self.read_register_region(SFE_OPT4048_REGISTER_INT_CONTROL, buff)
+        ret_val = self._i2c.readWord(self.address, SFE_OPT4048_REGISTER_INT_CONTROL)
 
         if ret_val != 0:
             return False
@@ -329,7 +333,7 @@ class QwOpt4048:
         buff[0] = int_reg >> 8
         buff[1] = int_reg
 
-        ret_val = self.write_register_region(SFE_OPT4048_REGISTER_INT_CONTROL, buff)
+        ret_val = self._i2c.writeWord(SFE_OPT4048_REGISTER_INT_CONTROL
 
         if ret_val != 0:
             return False
@@ -339,7 +343,7 @@ class QwOpt4048:
 
     def get_int_mechanism(self):
         buff = bytearray([0, 0])
-        self.read_register_region(SFE_OPT4048_REGISTER_INT_CONTROL, buff)
+        self._i2c.readWord(self.address, SFE_OPT4048_REGISTER_INT_CONTROL)
 
         int_reg = (buff[0] << 8) | buff[1]
 
@@ -348,7 +352,7 @@ class QwOpt4048:
 
     def get_all_flags(self):
         buff = bytearray([0, 0])
-        self.read_register_region(SFE_OPT4048_REGISTER_FLAGS, buff)
+        self._i2c.readWord(self.address, SFE_OPT4048_REGISTER_FLAGS)
 
         flag_reg = (buff[0] << 8) | buff[1]
 
@@ -386,7 +390,7 @@ class QwOpt4048:
 
     def set_fault_count(self, count):
         buff = [0, 0]
-        retVal = self.readRegisterRegion(SFE_OPT4048_REGISTER_CONTROL, buff)
+        retVal = self._i2c.readWord(self.address, SFE_OPT4048_REGISTER_CONTROL)
 
         if retVal != 0:
             return False
@@ -397,7 +401,7 @@ class QwOpt4048:
         buff[0] = (controlReg >> 8) & 0xFF
         buff[1] = controlReg & 0xFF
 
-        retVal = self.writeRegisterRegion(SFE_OPT4048_REGISTER_CONTROL, buff)
+        retVal = self.writeRegisterRegion(SFE_OPT4048_REGISTER_CONTROL
 
         if retVal != 0:
             return False
@@ -408,7 +412,7 @@ class QwOpt4048:
         buff = [0, 0]
         controlReg = 0
 
-        self.readRegisterRegion(SFE_OPT4048_REGISTER_CONTROL, buff)
+        self._i2c.readWord(self.address, SFE_OPT4048_REGISTER_CONTROL)
 
         controlReg = (buff[0] << 8) | buff[1]
 
@@ -419,7 +423,7 @@ class QwOpt4048:
             return False
 
         buff = bytearray([0, 0])  # Assuming buff is of type uint8_t[2]
-        retVal = self.write_register_region(SFE_OPT4048_REGISTER_THRESH_L_EXP_RES, buff)
+        retVal = self._i2c.writeWord(SFE_OPT4048_REGISTER_THRESH_L_EXP_RES
 
         if retVal != 0:
             return False
@@ -431,7 +435,7 @@ class QwOpt4048:
         buff = bytearray([0, 0])  # Assuming buff is of type uint8_t[2]
         threshReg = opt4048_reg_thresh_exp_res_low_t()
 
-        self.read_register_region(SFE_OPT4048_REGISTER_THRESH_L_EXP_RES, buff)
+        self._i2c.readWord(self.address, SFE_OPT4048_REGISTER_THRESH_L_EXP_RES)
 
         threshReg.word = (buff[0] << 8) | buff[1]
 
@@ -442,7 +446,7 @@ class QwOpt4048:
 
     def set_i2c_burst(self, enable):
         buff = bytearray([0, 0])  # Assuming buff is of type uint8_t[2]
-        retVal = self.read_register_region(SFE_OPT4048_REGISTER_INT_CONTROL, buff)
+        retVal = self._i2c.readWord(self.address, SFE_OPT4048_REGISTER_INT_CONTROL)
 
         if retVal != 0:
             return False
@@ -455,7 +459,7 @@ class QwOpt4048:
         buff[0] = intReg.word >> 8
         buff[1] = intReg.word
 
-        retVal = self.write_register_region(SFE_OPT4048_REGISTER_INT_CONTROL, buff)
+        retVal = self._i2c.writeWord(SFE_OPT4048_REGISTER_INT_CONTROL
 
         if retVal != 0:
             return False
@@ -467,7 +471,7 @@ class QwOpt4048:
         buff = bytearray([0, 0])  # Assuming buff is of type uint8_t[2]
         intReg = opt4048_reg_int_control_t()
 
-        self.read_register_region(SFE_OPT4048_REGISTER_INT_CONTROL, buff)
+        self._i2c.readWord(self.address, SFE_OPT4048_REGISTER_INT_CONTROL)
 
         intReg.word = (buff[0] << 8) | buff[1]
 
@@ -479,8 +483,8 @@ class QwOpt4048:
         adc_code = 0
         mantissa = 0
 
-        adc_reg = self.read_register_region(SFE_OPT4048_REGISTER_EXP_RES_CH0, buff, 4)
-        adc1_reg = self.read_register_region(SFE_OPT4048_REGISTER_RES_CNT_CRC_CH0, buff, 4)
+        adc_reg = self._i2c.readWord(self.address, SFE_OPT4048_REGISTER_EXP_RES_CH04)
+        adc1_reg = self._i2c.readWord(self.address, SFE_OPT4048_REGISTER_RES_CNT_CRC_CH04)
 
         mantissa = (adc_reg.result_msb_ch0 << 8) | adc1_reg.result_lsb_ch0
 
@@ -493,8 +497,8 @@ class QwOpt4048:
         adc_code = 0
         mantissa = 0
 
-        adc_reg = self.read_register_region(SFE_OPT4048_REGISTER_EXP_RES_CH1, buff, 4)
-        adc1_reg = self.read_register_region(SFE_OPT4048_REGISTER_RES_CNT_CRC_CH1, buff, 4)
+        adc_reg = self._i2c.readWord(self.address, SFE_OPT4048_REGISTER_EXP_RES_CH14)
+        adc1_reg = self._i2c.readWord(self.address, SFE_OPT4048_REGISTER_RES_CNT_CRC_CH14)
 
         mantissa = (adc_reg.result_msb_ch1 << 8) | adc1_reg.result_lsb_ch1
 
@@ -507,8 +511,8 @@ class QwOpt4048:
         adc_code = 0
         mantissa = 0
 
-        adc_reg = self.read_register_region(SFE_OPT4048_REGISTER_EXP_RES_CH2, buff, 4)
-        adc1_reg = self.read_register_region(SFE_OPT4048_REGISTER_RES_CNT_CRC_CH2, buff, 4)
+        adc_reg = self._i2c.readWord(self.address, SFE_OPT4048_REGISTER_EXP_RES_CH24)
+        adc1_reg = self._i2c.readWord(self.address, SFE_OPT4048_REGISTER_RES_CNT_CRC_CH24)
 
         mantissa = (adc_reg.result_msb_ch2 << 8) | adc1_reg.result_lsb_ch2
 
@@ -521,14 +525,99 @@ class QwOpt4048:
         adc_code = 0
         mantissa = 0
 
-        adc_reg = self.read_register_region(SFE_OPT4048_REGISTER_EXP_RES_CH3, buff, 4)
-        adc1_reg = self.read_register_region(SFE_OPT4048_REGISTER_RES_CNT_CRC_CH3, buff, 4)
+        adc_reg = self._i2c.readWord(self.address, SFE_OPT4048_REGISTER_EXP_RES_CH34)
+        adc1_reg = self._i2c.readWord(self.address, SFE_OPT4048_REGISTER_RES_CNT_CRC_CH34)
 
         mantissa = (adc_reg.result_msb_ch3 << 8) | adc1_reg.result_lsb_ch3
 
         adc_code = mantissa << adc_reg.exponent_ch3
 
         return adc_code   # Add other methods here...
+
+    def get_all_adc():
+        color = self.sfe_color_t()
+        color.red = get_adc_ch0()
+        color.green = get_adc_ch1()
+        color.blue = get_adc_ch2()
+        color.white = get_adc_ch3()
+        return color
+
+
+    def get_all_channel_data():
+        buff = read_register_region(SFE_OPT4048_REGISTER_EXP_RES_CH0, 16)
+        
+        adc0MSB = opt4048_reg_exp_res_ch0_t((buff[0] << 8) | buff[1])
+        adc0LSB = opt4048_reg_res_cnt_crc_ch0_t((buff[2] << 8) | buff[3])
+        
+        adc1MSB = opt4048_reg_exp_res_ch1_t((buff[4] << 8) | buff[5])
+        adc1LSB = opt4048_reg_res_cnt_crc_ch1_t((buff[6] << 8) | buff[7])
+        
+        adc2MSB = opt4048_reg_exp_res_ch2_t((buff[8] << 8) | buff[9])
+        adc2LSB = opt4048_reg_res_cnt_crc_ch2_t((buff[10] << 8) | buff[11])
+        
+        adc3MSB = opt4048_reg_exp_res_ch3_t((buff[12] << 8) | buff[13])
+        adc3LSB = opt4048_reg_res_cnt_crc_ch3_t((buff[14] << 8) | buff[15])
+
+        mantissa_ch0 = adc0MSB.result_msb_ch0 << 8 | adc0LSB.result_lsb_ch0
+        adc_code_ch0 = mantissa_ch0 << adc0MSB.exponent_ch0
+        
+        mantissa_ch1 = adc1MSB.result_msb_ch1 << 8 | adc1LSB.result_lsb_ch1
+        adc_code_ch1 = mantissa_ch1 << adc1MSB.exponent_ch1
+        
+        mantissa_ch2 = adc2MSB.result_msb_ch2 << 8 | adc2LSB.result_lsb_ch2
+        adc_code_ch2 = mantissa_ch2 << adc2MSB.exponent_ch2
+        
+        mantissa_ch3 = adc3MSB.result_msb_ch3 << 8 | adc3LSB.result_lsb_ch3
+        adc_code_ch3 = mantissa_ch3 << adc3MSB.exponent_ch3
+        
+        color = sfe_color_t(
+            red=adc_code_ch0, green=adc_code_ch1, blue=adc_code_ch2, white=adc_code_ch3,
+            counterR=adc0LSB.counter_ch0, counterG=adc1LSB.counter_ch1,
+            counterB=adc2LSB.counter_ch2, counterW=adc3LSB.counter_ch3,
+            CRCR=adc0LSB.crc_ch0, CRCG=adc1LSB.crc_ch1, CRCB=adc2LSB.crc_ch2, CRCW=adc3LSB.crc_ch3
+        )
+
+    return color
+    def get_lux(self):
+        adc_ch1 = self.get_adc_ch1()
+        lux = adc_ch1 * self.cie_matrix[1][3]
+        return lux
+
+    def get_CIEX(self):
+        x, y, z = 0, 0, 0
+        cie_x = 0
+        color = sfe_color_t()
+
+        self.get_all_channel_data(color)
+
+        for row in range(self.OPT_MATRIX_ROWS):
+            x += color.red * self.cie_matrix[row][0]
+            y += color.green * self.cie_matrix[row][1]
+            z += color.blue * self.cie_matrix[row][2]
+
+        total = x + y + z
+        if total != 0:
+            cie_x = x / total
+
+        return cie_x
+
+    def get_CIEY(self):
+        x, y, z = 0, 0, 0
+        cie_y = 0
+        color = sfe_color_t()
+
+        self.get_all_channel_data(color)
+
+        for row in range(self.OPT_MATRIX_ROWS):
+            x += color.red * self.cie_matrix[row][0]
+            y += color.green * self.cie_matrix[row][1]
+            z += color.blue * self.cie_matrix[row][2]
+
+        total = x + y + z
+        if total != 0:
+            cie_y = y / total
+
+        return cie_y
 
     def get_cct(self):
         CIEx = self.get_CIEx()
